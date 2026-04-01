4eceb1a5c9b4e735dd76dedbf54732db4eeff67f
Alok Kumar <125150272+iamalok73@users.noreply.github.com>
2026-04-01 17:26:35 +0530

    Add files via upload

---
+++ formula.py
@@ -0,0 +1,11 @@
+def list_sum(l):
+    s = 0
+    for i in l:
+        s +=i
+    print(f"Total Sum is {s}")
+
+def count_length(l):
+    c = 0
+    for i in l:
+        c+=1
+    print(f"Total Length of list is {c}")
---
+++ mod_file.py
@@ -0,0 +1,15 @@
+print("this file is state that user defind modules.")
+
+
+# ik aise function jha pr ik jaise log sotre hote hai.
+
+
+def Alok():
+    print("Alok is Data Science Trainner.")
+
+def Rakhi():
+    print("Rakhi Specilization in Stock marketing.,")
+
+def Sarad():
+    print("Sarad sir is Full Stack Trainner.")
+
